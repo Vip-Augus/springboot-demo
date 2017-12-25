30ca43461c1bac63b1bd9e23e474b494f84f6f72
Vip-Augus <850366301@qq.com>
2017-12-25 18:37:53 +0800

    文件输入输出

---
+++ src/main/java/com/example/demo/util/MinioFileUtils.java
@@ -0,0 +1,17 @@
+package com.example.demo.util;
+
+import com.example.demo.config.MinioConfigBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Minio文件存储工具
+ * Author JingQ on 2017/12/25.
+ */
+public class MinioFileUtils {
+
+    @Autowired
+    private MinioConfigBean minioConfigBean;
+
+
+
+}
