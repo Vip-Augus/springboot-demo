0ae5318ceecb00cb079fb339fe6c54f3df6f8e7f
Vip-Augus <850366301@qq.com>
2017-12-25 18:37:53 +0800

    文件输入输出

---
+++ src/main/java/com/example/demo/config/MinioConfigBean.java
@@ -0,0 +1,44 @@
+package com.example.demo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Author JingQ on 2017/12/25.
+ */
+@Configuration
+@ConfigurationProperties(prefix = "minio")
+@PropertySource("classpath:application.properties")
+public class MinioConfigBean {
+
+    private String ip;
+
+    private String accessKey;
+
+    private String secretKey;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+}
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -6,8 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 /**
  * Author JingQ on 2017/12/20.
@@ -35,6 +45,59 @@ public class TestController {
         user.setType(0);
         user.setIdNumber("2014327101027");
         userServiceImpl.insert(user);
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public void testUpload(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            String fileName = file.getOriginalFilename();
+            String filePath = "D://test/";
+            File saveFile = new File(filePath + fileName);
+            if (!saveFile.getParentFile().exists()) {
+                saveFile.getParentFile().mkdirs();
+            }
+            try {
+                file.transferTo(saveFile);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public void downFile(HttpServletRequest request, HttpServletResponse response) {
+        String filePath = "D://test/";
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        String fileName = "告白气球1.png";
+        String target = "F://1.png";
+        try {
+            File file = new File(filePath + fileName);
+            if (!file.exists()) {
+                throw new Exception("文件不存在");
+            }
+            bis = new BufferedInputStream(new FileInputStream(file));
+            bos = new BufferedOutputStream(new FileOutputStream(target));
+            byte[] buff = new byte[2048];
+            int bytesRead;
+            while (-1 != (bytesRead = bis.read(buff, 0, buff.length))) {
+                bos.write(buff, 0, bytesRead);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bis != null) {
+                    bis.close();
+                }
+                if (bos != null) {
+                    bos.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
